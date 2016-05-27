c00483eaa23c17af2e5fbf868b68f2abb96dd85a
leopardiwe <ruslandovg291@gmail.com>
2016-05-27 13:03:25 +0300

    init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://checkout/admin/');
+define('HTTP_CATALOG', 'http://checkout/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://checkout/admin/');
+define('HTTPS_CATALOG', 'http://checkout/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/checkout/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/checkout/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/checkout/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/checkout/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/checkout/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/checkout/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/checkout/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/checkout/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/checkout/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/checkout/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/checkout/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/checkout/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'checkout');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://checkout/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://checkout/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/checkout/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/checkout/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/checkout/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/checkout/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/checkout/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/checkout/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/checkout/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/checkout/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/checkout/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/checkout/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/checkout/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'checkout');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
